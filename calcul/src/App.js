a305d8e378305f5fa315a7f6ec622eff964e09ea
AshTorosyan <torosyanashotjs@gmail.com>
2022-07-14 14:51:39 +0400

    components for Calculator

---
+++ calcul/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Button from "./Components/Button/Button";
+import Wrapper from "./UI/Wrapper/Wrapper";
+
+
+function App() {
+
+
+
+  return (
+    <Wrapper>
+      
+    </Wrapper>
+  );
+}
+
+export default App;
